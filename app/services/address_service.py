3aa17dc7d24312dbb460ce5d74c4b90f332f6d18
Shashank <skankatti@gmail.com>
2026-03-17 01:14:35 +0530

    added update delete

--- app/services/address_service.py
+++ app/services/address_service.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from app.models.address_model import Address
+from app.schemas.address_schema import AddressCreate
 
 
-def create_address(db: Session, address):
+def create_address(db: Session, address: AddressCreate):
 
     new_address = Address(**address.dict())
 
@@ -11,3 +12,32 @@ def create_address(db: Session, address):
     db.refresh(new_address)
 
     return new_address
+
+
+def update_address(db: Session, address_id: int, address_data: AddressCreate):
+
+    address = db.query(Address).filter(Address.id == address_id).first()
+
+    if not address:
+        return None
+
+    for key, value in address_data.dict().items():
+        setattr(address, key, value)
+
+    db.commit()
+    db.refresh(address)
+
+    return address
+
+
+def delete_address(db: Session, address_id: int):
+
+    address = db.query(Address).filter(Address.id == address_id).first()
+
+    if not address:
+        return False
+
+    db.delete(address)
+    db.commit()
+
+    return True
